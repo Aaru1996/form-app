302b55dbcb45ee29b579c48f717958dc0168f100
Arun Kumar Yadav <arunkumaryadav19031996@gmail.com>
2024-06-24 14:59:40 +0530

    second commit

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
-const Display = () =>{
+const Display = ({data}) =>{
+
+    console.log(data)
 
     return (
-        <div>
-            Display
-        </div>
+        <table border={1}>
+                  <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                        <th>Date</th>
+                    </tr>
+                  </thead>
+                  {data?.map((e)=>{
+                        return (
+                            <tbody key={e.id}>
+                                <tr>
+                                    <td>{e.username}</td>
+                                    <td>{e.age}</td>
+                                    <td>{e.gender}</td>
+                                    <td>{e.date}</td>
+                                </tr>
+                            </tbody>
+                        )
+                  })}
+        </table>
     )
 }
 
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Display from './Display';
 
 const Form = () => {
+    const [form , setForm] = useState({
+          username:"",
+          age : "",
+          gender : 'female',
+          date : ''
+    })
+    const [formData, setFormData] = useState([]);
+
+    const refUser=useRef();
+
+    const handleChange = (e) =>{
+        const {name, value} = e.target;
+        setForm({...form, [name]:value})
+        // console.log(form)
+    }
+
+    const handleSubmit = (e) =>{
+              e.preventDefault()
+              console.log(form)
+
+              if(!form.username){
+              refUser.current.focus()
+              return 
+              }
+              if(!form.age){
+                alert('fill your age')
+                return
+              }
+              if(!form.date){
+                alert('fill the date')
+                return
+              }
+
+              setFormData([...formData, {id:Date.now(), ...form}])
+              setForm({
+                username:"",
+                age : "",
+                gender : 'female',
+                date : ''
+              })
+    }
 
     return(
-        <form>
-            form
+        <>
+        <form onSubmit={handleSubmit}>
+            <input type='text' ref={refUser} name='username' value={form.username} placeholder='type Name' onChange={handleChange}/><br/>
+            <input type='number' name='age' value={form.age} placeholder='Enter age' onChange={handleChange} /><br/>
+            <span>Select your gender {" "}</span>
+            <select name='gender' value={form.gender}   onChange={handleChange}>
+                <option value="male">male</option>
+                <option value="female">female</option>
+            </select><br/>
+            <input type='date' name='date' value={form.date} onChange={handleChange} /><br/>
+            <input type='submit' value='Login' />
         </form>
+        <br/>
+        <Display data={formData}/>
+        </>
     )
 }
 
